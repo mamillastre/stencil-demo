074300fbf7c893b7aed30d7b1f12f8d5ce961109
Maxime Amillastre <maxime.amillastre@orange.com>
2025-02-12 16:57:32 +0100

    add the angular library

---
+++ packages/angular/projects/rdr-elements/src/lib/rdr-elements.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-rdr-elements',
+  imports: [],
+  template: `
+    <p>
+      rdr-elements works!
+    </p>
+  `,
+  styles: ``
+})
+export class RdrElementsComponent {
+
+}
---
+++ packages/angular/projects/rdr-elements/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of rdr-elements
+ */
+
+export * from './lib/rdr-elements.service';
+export * from './lib/rdr-elements.component';
